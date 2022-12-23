7998ec0a334c73faa7c2de30e1e93494bb023bff
Dittrich <pdittrich@GK-DOMAIN>
2022-12-23 08:58:14 +0100

    save for restart

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -2,14 +2,22 @@ import java.rmi.AlreadyBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import rmi.MessengerServiceImpl;
 
 public class Server extends MessengerServiceImpl {
 	public Server() throws RemoteException {
 	}
 
 	public static void main(String args[]) throws RemoteException, AlreadyBoundException {
+		ResourceBundle serverProperties = ResourceBundle.getBundle("Entry.serverConfig", Locale.GERMANY);
+		int serverPort = Integer.valueOf(serverProperties.getString("port"));
+		String lookupName = serverProperties.getString("lookup_name");
+
 		MessengerServiceImpl messengerServiceImpl = new MessengerServiceImpl();
-		Registry registry = LocateRegistry.createRegistry(5555);
-		registry.bind("RMI_EXAMPLE", messengerServiceImpl);
+		Registry registry = LocateRegistry.createRegistry(serverPort);
+		registry.bind(lookupName, messengerServiceImpl);
 	}
 }
